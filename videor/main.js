868153afe6c4bb702c24cdacfde0eb483694c030
Administrator <Administrator@MinhHoang>
2021-03-21 22:13:49 +0700

    Update

---
+++ videor/main.js
@@ -0,0 +1,5 @@
+// load.timeout = 1000;
+load.removeLoadWhenDone = true;
+// load.log = true;
+// load.basePath = 'http://dangminhhoang.com/videor';
+load([true, 'lib/jquery.min.js', true, 'lib/underscore-min.js', 'model/controller']);
