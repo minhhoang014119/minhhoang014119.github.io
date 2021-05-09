4d32057f6458a61e3f32f82766a05b1fcca10a6b
Administrator <Administrator@MinhHoang>
2021-05-09 13:40:40 +0700

    Update

--- audior/main.js
+++ audior/main.js
@@ -1,7 +1,4 @@
-// load.timeout = 1000;
-load.removeLoadWhenDone = true;
-// load.log = true;
-// load.basePath = 'http://dangminhhoang.com/videor';
+load.timeout = 100;
 load(['../lib/jquery.min.js', '../lib/underscore-min.js', 'model/controller'], function(a, b, Controller) {
 	new Controller();
 });
