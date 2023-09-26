5a41e9455336398caa364dd77db90d8d7adec193
admin <admin@test.com>
2023-09-27 00:59:39 +0700

    audior

---
+++ audior/lib/Widget.js
@@ -0,0 +1,15 @@
+define([], () => {
+  return class Widget {
+    events() { }
+    init() { }
+    constructor() { this.init(); this.events(); }
+    val_field(field, name, val) { field = this[field] || field; if (val == undefined) return field[name]; field[name] = val; return this; }
+    flag_field(field, name, val) { return this.val_field(field, name, val == undefined ? !this.val_field(field, name) : val); }
+    plus_field(field, name, val) { return this.val_field(field, name, val == undefined ? undefined : this.val_field(field, name) + val); }
+    val_prop(prop, val) { if (val == undefined) return this[prop]; this[prop] = val; return this; }
+    flag_prop(prop, val) { return this.val_prop(prop, val == undefined ? !this.val_prop(prop) : val); }
+    plus_prop(prop, val) { return this.val_prop(prop, val == undefined ? undefined : this.val_prop(prop) + val); }
+    toggle_el(el, val) { (this[el] || el).toggle(val); return this; }
+    is_display(el) { return (this[el] || el).is(':visible'); }
+  }
+})
--- audior/main.js
+++ audior/main.js
@@ -1,4 +1,15 @@
-require(['../lib/jquery.min', '../lib/underscore-min', './model/controller'], (a, b, Controller) => {
+const root = location.href.replace(/[^\/]*$/, '')
+define('lib', {
+  load: (name, req, onLoad) => {
+    require([root + 'lib/' + name + '.js'], onLoad)
+  }
+})
+define('model', {
+  load: (name, req, onLoad) => {
+    require([root + 'model/' + name + '.js'], onLoad)
+  }
+})
+require(['lib!jquery.min', 'lib!underscore-min', './model/controller'], (a, b, Controller) => {
   const audior = new Controller()
   window.getAudior && window.getAudior(audior)
 })
