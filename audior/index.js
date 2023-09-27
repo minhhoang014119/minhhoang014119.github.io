9c9c9dbc02a43e03eb925e2581cf64fe0b3fd243
admin <admin@test.com>
2023-09-27 22:04:09 +0700

    audior

--- audior/index.js
+++ audior/index.js
@@ -1,11 +1,11 @@
 const root = location.href.replaceAll('index.html', '').replace(/\/$/, '')
 define('lib', {
   load: (name, req, onLoad) => {
-    require([root + '/../lib/' + name + '.js'], onLoad)
+    require([root + '/../lib/' + name + '.js'], mod => define(name, mod) || onLoad(mod))
   }
 })
 require(['lib!css', 'lib!jquery.min', 'lib!underscore-min', './model/controller'], (css, jquery, underscore, Controller) => (async () => {
-  require(['css!../../videor/css/style.css'])
+  require(['css!../videor/css/style.css'])
   await new Promise((res, interval) => (interval = setInterval(() => window.$ && window._ && res(clearInterval(interval)), 100)))
   const audior = new Controller()
   window.getAudior && window.getAudior(audior)
