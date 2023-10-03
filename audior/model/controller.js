eac93d6e5ed6e7748dfafe9d67205378d96e8175
admin <admin@test.com>
2023-10-03 23:01:37 +0700

    audior/model/subText.js

--- audior/model/controller.js
+++ audior/model/controller.js
@@ -1,4 +1,4 @@
-define(['./subText', '../../videor/model/controller'], (SubText, SuperController) => {
+define(['../../videor/model/controller'], SuperController => {
   return class Controller extends SuperController {
     get_file_extention() { return 'mp3'; }
   }
